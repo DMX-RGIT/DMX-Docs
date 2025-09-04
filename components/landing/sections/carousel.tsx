4b887de9ef722423cda0ddd0d72cb03af9f1026a
gitcode <website0wordpress@gmail.com>
2025-09-04 19:35:14 +0530

    fix: carousel slidesPerScreen

--- components/landing/sections/carousel.tsx
+++ components/landing/sections/carousel.tsx
@@ -4,14 +4,13 @@ import {
   CarouselNavigation,
   CarouselItem,
 } from "@/components/ui/slider";
-import { cn } from "@/lib/utils";
 
 // ref: https://motion-primitives.com/docs/carousel#component-api
 
 interface CarouselSpacingProps {
   slides: string[];
   video?: boolean;
-  slidesPerScreen?: string;
+  slidesPerScreen?: number;
 }
 
 export function Slider({
@@ -19,15 +18,26 @@ export function Slider({
   video = false,
   slidesPerScreen,
 }: CarouselSpacingProps) {
+  // for some reason putting slidesPerScreen directly in className dosent work, due to tailwind ignores runtime variables
+  function getBasisClass(slidesPerScreen?: number): string {
+    const map: Record<number, string> = {
+      1: "basis-1/1 pl-4",
+      2: "basis-1/2 pl-4",
+      3: "basis-1/3 pl-4",
+      4: "basis-1/4 pl-4",
+      5: "basis-1/5 pl-4",
+      6: "basis-1/6 pl-4",
+    };
+    return map[slidesPerScreen ?? 1] ?? "basis-1/1 pl-4";
+  }
+  const className = getBasisClass(slidesPerScreen);
+
   return (
     <div className="relative w-full px-4 pb-16">
       <Carousel>
         <CarouselContent className="-ml-4">
           {slides.map((src, index) => (
-            <CarouselItem
-              key={index}
-              className={cn(`basis-1/${slidesPerScreen ?? "1"} pl-4`)}
-            >
+            <CarouselItem key={index} className={className}>
               <div className="relative flex aspect-auto items-center justify-center overflow-hidden border border-zinc-200 dark:border-zinc-800 rounded-xl">
                 {video ? (
                   <video
