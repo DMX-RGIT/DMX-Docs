df4f32174cac62afd6a276c730bf6d069b9a9cda
gitcode <website0wordpress@gmail.com>
2025-09-18 17:46:24 +0530

    padding set

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -380,8 +380,8 @@ export function SidebarFolderContent(props: CollapsibleContentProps) {
         "relative",
         "-mt-1.5 mb-2",
         level === 1 && [
-          "before:content-[''] before:absolute before:w-px before:inset-y-1 before:bg-fd-border before:start-2.5",
-          "**:data-[active=true]:before:content-[''] **:data-[active=true]:before:bg-fd-primary **:data-[active=true]:before:absolute **:data-[active=true]:before:w-px **:data-[active=true]:before:inset-y-2.5 **:data-[active=true]:before:start-2.5",
+          "before:content-[''] before:absolute before:w-px before:inset-y-1 before:bg-fd-border before:start-3",
+          "**:data-[active=true]:before:content-[''] **:data-[active=true]:before:bg-fd-primary **:data-[active=true]:before:absolute **:data-[active=true]:before:w-px **:data-[active=true]:before:inset-y-2.5 **:data-[active=true]:before:start-3",
         ],
         props.className
       )}
